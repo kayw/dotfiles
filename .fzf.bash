ff1415a0d9ddd1c123f4b3f6cef47130b94ff940
kayw <kayw@welogix.cn>
2018-11-08 21:53:01 +0800

    vim ale / remove youcompleteme#Enable / gitconfig mr email

--- .fzf.bash
+++ .fzf.bash
@@ -4,12 +4,6 @@ if [[ ! "$PATH" == */home/kayw/.vim/bundle/fzf/bin* ]]; then
   export PATH="$PATH:/home/kayw/.vim/bundle/fzf/bin"
 fi
 
-# Man path
-# --------
-if [[ ! "$MANPATH" == */home/kayw/.vim/bundle/fzf/man* && -d "/home/kayw/.vim/bundle/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/home/kayw/.vim/bundle/fzf/man"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/home/kayw/.vim/bundle/fzf/shell/completion.bash" 2> /dev/null
